7c634e1da538c647e10081b858948d4c1232a86d
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-07-26 12:19:18 +0300

    added navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+
+function NavBar() {
+    return (
+      <nav>
+        <ul>
+          <li>cocktail</li>
+        </ul>
+      </nav>
+    )
+  }
+
+export default NavBar
